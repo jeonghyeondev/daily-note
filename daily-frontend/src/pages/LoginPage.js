b5526a106119be00e5fead1b540359012ddde187
kkkkkjjjjjh <rlwjgu12@gmail.com>
2021-01-02 21:46:37 +0900

    helmet meta태그 설정

--- daily-frontend/src/pages/LoginPage.js
+++ daily-frontend/src/pages/LoginPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import AuthTemplate from '../components/auth/AuthTemplate';
 import LoginForm from '../containers/auth/LoginForm';
+import { Helmet } from 'react-helmet-async';
 
 // 로그인 페이지
 const LoginPage = () => {
   return (
     <AuthTemplate>
+      <Helmet>
+        <title>로그인 - DAILY NOTE</title>
+      </Helmet>
       <LoginForm />
     </AuthTemplate>
   );
--- daily-frontend/src/pages/RegisterPage.js
+++ daily-frontend/src/pages/RegisterPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import AuthTemplate from '../components/auth/AuthTemplate';
 import RegisterForm from '../containers/auth/RegisterForm';
+import { Helmet } from 'react-helmet-async';
 
 // 회원가입 페이지
 const RegisterPage = () => {
   return (
     <AuthTemplate>
+      <Helmet>
+        <title>회원가입 - DAILY NOTE</title>
+      </Helmet>
       <RegisterForm />
     </AuthTemplate>
   );
